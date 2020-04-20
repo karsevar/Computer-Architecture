95069726fe1adbeb8db845e7ce760336a0d87ce8
masonkarsevar <masonkarsevar@gmail.com>
2020-04-20 12:11:08 -0700

    self.ram,self.reg, and self.pc initialized and ram_read and ram_write finished but untested

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,17 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # create the ram array that will have a total of 256 
+        # spaces because of the structure of the cpu in the spec 
+        self.ram = [0] * 256 
+
+        # create a register array that will carry all variables 
+        # that need to be executed for every opcode instruction 
+        self.reg = [0] * 8 
+
+        # create a pc counter variable that will be initialized to
+        # zero
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -59,6 +69,24 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+
+    def ram_write(self, value, address):
+        # this function accepts a value and an address to which the 
+        # value will be stored in the ram array.
+
+        # store the value into self.ram through self.ram[address] = value 
+
+        # return a message that the insertion was a success
+
+        self.ram[address] = value 
+        print(f'value {value} has been stored at ram position {address}') 
+
+    def ram_read(self, address):
+        # This function will take in an address (either in binary or 
+        # base 10) and return the value stored in the ram in that specific 
+        # adress 
+
+        return self.ram[address]
 
     def run(self):
         """Run the CPU."""
